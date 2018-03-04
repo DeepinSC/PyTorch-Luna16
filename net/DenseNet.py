3a00aa542db59ff664382a128a27fcc7df95ceb6
Rick <654834341@qq.com>
2018-03-04 19:23:16 +0800

    Add Bottleneck Layer

--- net/DenseNet.py
+++ net/DenseNet.py
@@ -2,6 +2,40 @@ import torch
 from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
+import torchvision
+
+class Bottleneck(nn.Module):
+    def __init__(self, in_plane, growth_rate, bn_size, drop_rate):
+        super(Bottleneck, self).__init__()
+        self.bn1 = nn.BatchNorm2d(in_plane)
+        self.conv1 = nn.Conv2d(in_plane, bn_size * growth_rate, kernel_size=1, stride=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(bn_size * growth_rate)
+        self.conv2 = nn.Conv2d(bn_size * growth_rate, growth_rate, kernel_size=3, padding=1, bias=False)
+        self.drop_rate = drop_rate
+
+    def forward(self, x):
+        out = self.conv1(F.relu(self.bn1(x)))
+        out = self.conv2(F.relu(self.bn2(out)))
+        if self.drop_rate > 0:
+            out = F.dropout(out, p=self.drop_rate, training=self.training)
+        # connect out and x to [out,x]
+        out = torch.cat([out, x], 1)
+        return out
+
 
 class DenseNet(nn.Module):
-    pass
+    r"""Densenet-BC model class, based on
+        `"Densely Connected Convolutional Networks" <https://arxiv.org/pdf/1608.06993.pdf>`_
+
+        Args:
+            growth_rate (int) - how many filters to add each layer (`k` in paper)
+            block_config (list of 4 ints) - how many layers in each pooling block
+            num_init_features (int) - the number of filters to learn in the first convolution layer
+            bn_size (int) - multiplicative factor for number of bottle neck layers
+              (i.e. bn_size * k features in the bottleneck layer)
+            drop_rate (float) - dropout rate after each dense layer
+            num_classes (int) - number of classification classes
+    """
+    def __init__(self, growth_rate=32, block_config=(6, 12, 24, 16),
+                 num_init_features=64, bn_size=4, drop_rate=0, num_classes=1000):
+        super(DenseNet, self).__init__()
