424ebab48e3268a7e3d580ba188dcd12338cf668
Rick <654834341@qq.com>
2018-03-09 09:36:35 +0800

    DSB17 data processing

--- data_processing.py
+++ data_processing.py
@@ -5,7 +5,14 @@ import os
 import scipy.ndimage
 import matplotlib.pyplot as plt
 
-from skimage import measure, morphology
+from skimage.morphology import ball, disk, dilation, binary_erosion, remove_small_objects, erosion, closing, reconstruction, binary_closing
+from skimage.measure import label,regionprops, perimeter
+from skimage.morphology import binary_dilation, binary_opening
+from skimage.filters import roberts, sobel
+from skimage import measure, feature
+from skimage.segmentation import clear_border
+from skimage import data
+from scipy import ndimage as ndi
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
 # Some constants
@@ -16,6 +23,7 @@ patients = patients[1:]
 
 
 def load_scan(path):
+    # 子文件夹
     slices = [dicom.read_file(path + '/' + s) for s in os.listdir(path)]
     slices.sort(key=lambda x: float(x.ImagePositionPatient[2]))
     try:
@@ -56,14 +64,30 @@ def get_pixels_hu(slices):
 
 first_patient = load_scan(INPUT_FOLDER + patients[0])
 first_patient_pixels = get_pixels_hu(first_patient)
-plt.hist(first_patient_pixels.flatten(), bins=80, color='c')
-plt.xlabel("Hounsfield Units (HU)")
-plt.ylabel("Frequency")
+plt.hist(first_patient_pixels.flatten(), bins=80, color='g')
+plt.xlabel("亨氏单位 (HU)")
+plt.ylabel("频率")
 plt.show()
 
 # Show some slice in the middle
 plt.imshow(first_patient_pixels[80], cmap=plt.cm.gray)
 plt.show()
+
+
+def read_ct_scan(folder_name):
+    # Read the slices from the dicom file
+    slices = [dicom.read_file(folder_name + '/' + s) for s in os.listdir(folder_name)]
+
+    # Sort the dicom slices in their respective order
+    slices.sort(key=lambda x: int(x.InstanceNumber))
+
+    # Get the pixel values for all the slices
+    slices = np.stack([s.pixel_array for s in slices])
+    slices[slices == -2000] = 0
+    return slices
+
+
+first_patient_array = read_ct_scan(INPUT_FOLDER + patients[0])
 
 
 def resample(image, scan, new_spacing=[1, 1, 1]):
@@ -80,6 +104,7 @@ def resample(image, scan, new_spacing=[1, 1, 1]):
 
     return image, new_spacing
 
+
 pix_resampled, spacing = resample(first_patient_pixels, first_patient, [1,1,1])
 print("Shape before resampling\t", first_patient_pixels.shape)
 print("Shape after resampling\t", pix_resampled.shape)
@@ -89,6 +114,7 @@ def plot_3d(image, threshold=-300):
     # Position the scan upright,
     # so the head of the patient would be at the top facing the camera
     p = image.transpose(2, 1, 0)
+    p = p[:, :, ::-1]
 
     verts, faces = measure.marching_cubes_classic(p, threshold)
 
@@ -97,7 +123,7 @@ def plot_3d(image, threshold=-300):
 
     # Fancy indexing: `verts[faces]` to generate a collection of triangles
     mesh = Poly3DCollection(verts[faces], alpha=0.70)
-    face_color = [0.45, 0.45, 0.75]
+    face_color = [0.45, 0.45, 0.8]
     mesh.set_facecolor(face_color)
     ax.add_collection3d(mesh)
 
@@ -108,4 +134,104 @@ def plot_3d(image, threshold=-300):
     plt.show()
 
 
-plot_3d(pix_resampled, 400)
+# plot_3d(pix_resampled, 400)
+
+
+def get_segmented_lungs(im, plot=False):
+
+    """
+    This function segments the lungs from the given 2D slice.
+    """
+
+    if plot:
+        f, plots = plt.subplots(8, 1, figsize=(5, 40))
+    '''
+    Step 1: Convert into a binary image. 
+    '''
+    binary = im < 604
+    if plot:
+        plots[0].axis('off')
+        plots[0].imshow(binary, cmap=plt.cm.bone)
+    '''
+    Step 2: Remove the blobs connected to the border of the image.
+    '''
+    cleared = clear_border(binary)
+    if plot:
+        plots[1].axis('off')
+        plots[1].imshow(cleared, cmap=plt.cm.bone)
+    '''
+    Step 3: Label the image.
+    '''
+    label_image = label(cleared)
+    if plot:
+        plots[2].axis('off')
+        plots[2].imshow(label_image, cmap=plt.cm.bone)
+    '''
+    Step 4: Keep the labels with 2 largest areas.
+    '''
+    areas = [r.area for r in regionprops(label_image)]
+    areas.sort()
+    if len(areas) > 2:
+        for region in regionprops(label_image):
+            if region.area < areas[-2]:
+                for coordinates in region.coords:
+                    label_image[coordinates[0], coordinates[1]] = 0
+    binary = label_image > 0
+    if plot:
+        plots[3].axis('off')
+        plots[3].imshow(binary, cmap=plt.cm.bone)
+    '''
+    Step 5: Erosion operation with a disk of radius 2. This operation is 
+    seperate the lung nodules attached to the blood vessels.
+    '''
+    selem = disk(2)
+    binary = binary_erosion(binary, selem)
+    if plot:
+        plots[4].axis('off')
+        plots[4].imshow(binary, cmap=plt.cm.bone)
+    '''
+    Step 6: Closure operation with a disk of radius 10. This operation is 
+    to keep nodules attached to the lung wall.
+    '''
+    selem = disk(10)
+    binary = binary_closing(binary, selem)
+    if plot:
+        plots[5].axis('off')
+        plots[5].imshow(binary, cmap=plt.cm.bone)
+    '''
+    Step 7: Fill in the small holes inside the binary mask of lungs.
+    '''
+    edges = roberts(binary)
+    binary = ndi.binary_fill_holes(edges)
+    if plot:
+        plots[6].axis('off')
+        plots[6].imshow(binary, cmap=plt.cm.bone)
+    '''
+    Step 8: Superimpose the binary mask on the input image.
+    '''
+    get_high_vals = binary == 0
+    im[get_high_vals] = 0
+    if plot:
+        plots[7].axis('off')
+        plots[7].imshow(im, cmap=plt.cm.bone)
+
+    return im
+
+
+def segment_lung_from_ct_scan(ct_scan):
+    """
+
+    :param ct_scan: 3D肺部未分割图像
+    :return: 3D肺部分割图像
+    """
+    return np.asarray([get_segmented_lungs(slice) for slice in ct_scan])
+
+
+ct_scan = pix_resampled
+segmented_ct_scan = segment_lung_from_ct_scan(ct_scan)
+segmented_ct_scan2 = segment_lung_from_ct_scan(first_patient_array)
+
+plot_3d(segmented_ct_scan2, 0)
+plot_3d(segmented_ct_scan2, 604)
+plot_3d(segmented_ct_scan, 604)
+
