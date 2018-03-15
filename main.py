014c8ec5dd4a1b89f810f94ea11f82a447aedbab
Rick <654834341@qq.com>
2018-03-15 19:29:46 +0800

    FCN && FCN mini

--- main.py
+++ main.py
@@ -4,13 +4,16 @@ import torch.nn as nn
 import torch.optim as optim
 import numpy as np
 from models.densenet import DenseNet121
-from segment_models.lenet import LeNet
+from segment_models.FCN import FCN8s, FCN32s, FCN_mini
+from skimage import transform
 
 from keras import backend as K
 smooth = 1.
+
+
 # dice loss
 def dice_coef(y_true, y_pred):
-    y_true_f = K.flatten(y_true)
+    y_true_f = K.flatten(y_true) y_true.view()
     y_pred_f = K.flatten(y_pred)
     intersection = K.sum(y_true_f * y_pred_f)
     return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
@@ -39,14 +42,24 @@ train_mask = np.load(train_mask_path).astype(np.float32)
 train_x = Variable(torch.from_numpy(train_data))
 train_y = Variable(torch.from_numpy(train_mask))
 
+rescale = True
+
+if rescale:
+    train_data = np.resize(train_data, [train_data.shape[0], train_data.shape[1], 16, 16])
+    train_x = Variable(torch.from_numpy(train_data))
+    train_mask = np.resize(train_mask, [train_mask.shape[0], train_mask.shape[1], 16, 16])
+    train_y = Variable(torch.from_numpy(train_mask))
+
 # net = DenseNet121()
-net = LeNet(train_x.shape)
+net = FCN_mini()
 net.zero_grad()
 print('** Init finished **')
 
 output = net(train_x)
 print('** First output finished **')
-loss = dice_coef_loss(output, train_y)
+loss = dice_coef_loss(output.numpy(), train_y.numpy())
+#criterion = nn.CrossEntropyLoss()
+#loss = criterion(output, train_y)
 print(loss)
 
 optimizer = optim.SGD(net.parameters(), lr=0.01)
