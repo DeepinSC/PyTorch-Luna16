a6821a422d1946e553c1eca91c71d9ecb934371f
Rick <654834341@qq.com>
2018-03-17 20:24:25 +0800

    Add Densenet-FCN

--- data_processing/tutorial_code/LUNA_train_unet.py
+++ data_processing/tutorial_code/LUNA_train_unet.py
@@ -1,14 +1,14 @@
 from __future__ import print_function
 
+import math
 import numpy as np
-from keras.models import Model
-from keras.layers import Input, merge, Convolution2D, MaxPooling2D, UpSampling2D
-from keras.optimizers import Adam
-from keras.optimizers import SGD
 from keras.callbacks import ModelCheckpoint, LearningRateScheduler
 from keras import backend as K
 
-working_path = "/home/jonathan/tutorial/"
+from data_processing.models.unet import unet
+from data_processing.models.fcDensenet import DenseNetFCN
+
+working_path = "../../data/out/"
 
 K.set_image_dim_ordering('th')  # Theano dimension ordering in this code
 
@@ -36,52 +36,7 @@ def dice_coef_loss(y_true, y_pred):
     return -dice_coef(y_true, y_pred)
 
 
-def get_unet():
-    inputs = Input((1,img_rows, img_cols))
-    conv1 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(inputs)
-    conv1 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(conv1)
-    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
-
-    conv2 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(pool1)
-    conv2 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(conv2)
-    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
-
-    conv3 = Convolution2D(128, 3, 3, activation='relu', border_mode='same')(pool2)
-    conv3 = Convolution2D(128, 3, 3, activation='relu', border_mode='same')(conv3)
-    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
-
-    conv4 = Convolution2D(256, 3, 3, activation='relu', border_mode='same')(pool3)
-    conv4 = Convolution2D(256, 3, 3, activation='relu', border_mode='same')(conv4)
-    pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
-
-    conv5 = Convolution2D(512, 3, 3, activation='relu', border_mode='same')(pool4)
-    conv5 = Convolution2D(512, 3, 3, activation='relu', border_mode='same')(conv5)
-
-    up6 = merge([UpSampling2D(size=(2, 2))(conv5), conv4], mode='concat', concat_axis=1)
-    conv6 = Convolution2D(256, 3, 3, activation='relu', border_mode='same')(up6)
-    conv6 = Convolution2D(256, 3, 3, activation='relu', border_mode='same')(conv6)
-
-    up7 = merge([UpSampling2D(size=(2, 2))(conv6), conv3], mode='concat', concat_axis=1)
-    conv7 = Convolution2D(128, 3, 3, activation='relu', border_mode='same')(up7)
-    conv7 = Convolution2D(128, 3, 3, activation='relu', border_mode='same')(conv7)
-
-    up8 = merge([UpSampling2D(size=(2, 2))(conv7), conv2], mode='concat', concat_axis=1)
-    conv8 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(up8)
-    conv8 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(conv8)
-
-    up9 = merge([UpSampling2D(size=(2, 2))(conv8), conv1], mode='concat', concat_axis=1)
-    conv9 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(up9)
-    conv9 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(conv9)
-
-    conv10 = Convolution2D(1, 1, 1, activation='sigmoid')(conv9)
-
-    model = Model(input=inputs, output=conv10)
-
-    model.compile(optimizer=Adam(lr=1.0e-5), loss=dice_coef_loss, metrics=[dice_coef])
-
-    return model
-
-
+# How to use net?
 def train_and_predict(use_existing):
     print('-'*30)
     print('Loading and preprocessing train data...')
@@ -101,7 +56,11 @@ def train_and_predict(use_existing):
     print('-'*30)
     print('Creating and compiling model...')
     print('-'*30)
-    model = get_unet()
+    # model = unet()
+    model = DenseNetFCN(
+        input_shape=(1, img_rows, img_cols),
+        nb_dense_block=int(math.log(img_rows, 2)),
+    )
     # Saving weights to unet.hdf5 at checkpoints
     model_checkpoint = ModelCheckpoint('unet.hdf5', monitor='loss', save_best_only=True)
     #
@@ -121,7 +80,7 @@ def train_and_predict(use_existing):
     print('-'*30)
     print('Fitting model...')
     print('-'*30)
-    model.fit(imgs_train, imgs_mask_train, batch_size=2, nb_epoch=20, verbose=1, shuffle=True,
+    model.fit(imgs_train, imgs_mask_train, batch_size=2, epochs=20, verbose=1, shuffle=True,
               callbacks=[model_checkpoint])
 
     # loading best weights from training session
@@ -140,8 +99,8 @@ def train_and_predict(use_existing):
     np.save('masksTestPredicted.npy', imgs_mask_test)
     mean = 0.0
     for i in range(num_test):
-        mean+=dice_coef_np(imgs_mask_test_true[i,0], imgs_mask_test[i,0])
-    mean/=num_test
+        mean += dice_coef_np(imgs_mask_test_true[i,0], imgs_mask_test[i,0])
+    mean /= num_test
     print("Mean Dice Coeff : ",mean)
 
 
